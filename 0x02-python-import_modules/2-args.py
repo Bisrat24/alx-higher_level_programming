0a346bbb424e597aeb9949cdb17fb54a4e9b6959
Bisrat24 <58773706+Bisrat24@users.noreply.github.com>
2022-06-30 21:34:44 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    if int(len(sys.argv)) = 1:
+    print(len(sys.argv))
+    if int(len(sys.argv)) == 1:
         print("{:d} argument:".format(len(sys.argv) - 1))
     else:
         print("{:d} arguments:".format(len(sys.argv) - 1))
