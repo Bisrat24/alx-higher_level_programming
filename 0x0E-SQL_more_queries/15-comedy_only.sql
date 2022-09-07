9c0207970ec98b66d564047cf8431cd93fd98bb6
Bisrat24 <bisrattam@gmail.com>
2022-09-07 23:42:51 +0300

    sqlmore

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,6 @@
--- script that lists all Comedy shows in the database hbtn_0d_tvshows
-
-SELECT tv_shows.title
-FROM tv_shows
-    INNER JOIN tv_show_genres
-    ON tv_shows.id = tv_show_genres.show_id
-    INNER JOIN tv_genres
-    ON tv_show_genres.genre_id = tv_genres.id
+-- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+SELECT tv_shows.title AS title FROM tv_shows
+INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = "Comedy"
-ORDER BY tv_shows.title;
+ORDER BY title ASC;
