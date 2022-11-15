fe8c3024249fa1b41db3088d48d359da74018ce2
Bisrat24 <bisrattam@gmail.com>
2022-11-15 22:44:08 +0300

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,16 @@
 #!/usr/bin/node
-
-<<<<<<< HEAD
+/**
+ * Script that reads from a file
+ */
+const process = require('process');
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, data) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data);
-  }
-=======
+
+const filePath = process.argv[2];
+
 fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);} 
-  else {
-    console.log(data);}
->>>>>>> b557e951a912d4fea92f53ce8e03eb448d828185
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
 });
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,22 +1,35 @@
 #!/usr/bin/node
+/**
+ * Script that prints all characters of a Star Wars movie
+ */
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-let def = {};
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const abc = JSON.parse(body);
-    abc.characters.forEach(function (item, index, array) {
-      request(item, function (error, response, content) {
-        if (error) {
-          console.log(error);
-        } else {
-          def = JSON.parse(content);
-          console.log(def.name);
-        }
-      });
+const process = require('process');
+const filmId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+
+function characterRequest(characterUrl) {
+    return new Promise((resolve, reject) => {
+        request.get(characterUrl, (err, _resp, body) => {
+            if (err !== null) {
+                reject(err);
+            }
+            resolve(body);
+        });
     });
-  }
+}
+
+request.get(url, (err, _resp, body) => {
+    if (err === null) {
+        const film = JSON.parse(body);
+        const characters = film.characters;
+        characters.forEach((character) => {
+            characterRequest(character).then((body) => {
+                const characterInfo = JSON.parse(body);
+                console.log(characterInfo.name);
+            }).catch(err => console.log(err));
+        });
+    } else {
+        console.log(err);
+    }
 });
